15138395dc499e4ceb7066e8f340678af7558283
Victor Karpov <nilafe4@gmail.com>
2022-07-04 23:13:02 +0500

    refactor on flake8

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
+import os
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-import os
 from pathlib import Path
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from .newscrawll.lenta import parser_lenta_hacker_news
 from .newscrawll.habr import pick_all_titles_and_link_on_HABR
-
 
 
 app = FastAPI()
@@ -20,21 +19,17 @@ BASE_DIR = Path(__file__).resolve().parent
 templates = Jinja2Templates(directory=str(Path(BASE_DIR, 'templates')))
 
 
-
 @app.get("/", response_class=HTMLResponse)
 async def all_news(request: Request):
-	lenta_news = await parser_lenta_hacker_news()
-	habr_news = await pick_all_titles_and_link_on_HABR()
-	return templates.TemplateResponse(
-		"index.html",
-		{
-			'request': request,
-			'lenta': lenta_news,
-			'habr': habr_news,
-			}
-		)
+    lenta_news = await parser_lenta_hacker_news()
+    habr_news = await pick_all_titles_and_link_on_HABR()
+    return templates.TemplateResponse("index.html",
+                                      {
+                                          'request': request,
+                                          'lenta': lenta_news,
+                                          'habr': habr_news, })
 
 
 @app.get('/about', response_class=HTMLResponse)
 async def about(request: Request):
-	return templates.TemplateResponse('about.html', {'request': request,})
+    return templates.TemplateResponse('about.html', {'request': request})
--- newscrawll/habr.py
+++ newscrawll/habr.py
@@ -6,38 +6,28 @@ from lxml import etree
 from fake_useragent import UserAgent
 
 ua = UserAgent()
-headers = {'User-Agent':str(ua.random)}
+headers = {'User-Agent': str(ua.random)}
 HABR_NEWS = "https://habr.com/ru/news/"
 
 
 async def connect_site(session, url):
-	async with session.get(url) as res:
-		return await res.text()
+    async with session.get(url) as res:
+        return await res.text()
 
-ua = UserAgent()
-
-headers = {'User-Agent':str(ua.random)}
-
-
-
-HABR_NEWS="https://habr.com/ru/news/"
-
-async def connect_site(session, url):
-	async with session.get(url) as res:
-		return await res.text()
 
 async def pick_all_titles_and_link_on_HABR():
-	async with aiohttp.ClientSession(headers=headers) as session:
-		page_news = await connect_site(session, HABR_NEWS)
-		tree = etree.HTML(page_news)
-		titles = tree.xpath("//*[@class='tm-articles-list__item']//h2/a/span/text()")
-		links = ["https://habr.com" + link for link in tree.xpath("//*[@class='tm-articles-list__item']//h2/a/@href")]
-		time_date = tree.xpath("//*[@class='tm-articles-list__item']//time/text()")
-		news = {}
-		for i in range(len(titles)):
-			news[i+1] = {"title": titles[i], "link": links[i], "time": time_date[i]}
-		return news
-	
+    async with aiohttp.ClientSession(headers=headers) as session:
+        page_news = await connect_site(session, HABR_NEWS)
+        tree = etree.HTML(page_news)
+        titles = tree.xpath("//*[@class='tm-articles-list__item']//h2/a/span/text()")
+        links = ["https://habr.com" + link for link in tree.xpath("//*[@class='tm-articles-list__item']//h2/a/@href")]
+        time_date = tree.xpath("//*[@class='tm-articles-list__item']//time/text()")
+        news = {}
+        for i in range(len(titles)):
+            news[i+1] = {"title": titles[i], "link": links[i], "time": time_date[i]}
+            return news
+
+
 if __name__ == '__main__':
-	loop = asyncio.get_event_loop()
-	loop.run_until_complete(pick_all_titles_and_link_on_HABR())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(pick_all_titles_and_link_on_HABR())
